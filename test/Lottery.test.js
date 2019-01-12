a680c782a573d28c53fd63fdaa9a90a6e0fcdcc5
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-01-12 08:21:34 +0300

    Add test to send money and reset the players array

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -63,7 +63,7 @@ describe('Lottery Contract', () => {
         try {
             await lottery.methods.enter().send({
                 from: accounts[0],
-                value: 200 //in wi (less than 0.01 ether)
+                value: 200 //in wei (less than 0.01 ether)
             });
             assert(false);
         } catch (err) {
@@ -80,5 +80,19 @@ describe('Lottery Contract', () => {
         } catch (err) {
             assert(err);
         }
+    });
+
+    it('sends money to the winner and resets the players array', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('2', 'ether')
+        });
+
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+        await lottery.methods.pickWinner().send({ from: accounts[0] });
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+        const difference = finalBalance - initialBalance;
+
+        assert(difference > web3.utils.toWei('1.8', 'ether'));
     });
 });
