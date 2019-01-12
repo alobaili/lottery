f6a00566f198702e66c5508106e3b10697c740cd
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-01-12 06:52:48 +0300

    Initial commit
    This is just a duplicate of the inbox project with variable names
    corrected to represent the Lottery project. Lottery.test.js is cleaned
    in preperation to write new test code.

---
+++ deploy.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+const keys = require('./keys');
+
+const provider = new HDWalletProvider(
+    keys.mnemonic,
+    keys.infuraRinkebyURL
+);
+
+const web3 = new Web3(provider);
+
+// Add arbitrary function to enable the asunc-await syntax
+const deploy = async () => {
+
+    // Get list of accounts in HDWalletProvider
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    // Deploy contract
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    // Record the address of the deployed contract
+    console.log('Contract deployed to', result.options.address);
+};
+deploy();
